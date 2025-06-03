dc570a91c5a1e3cb4d5c58c2d77fff3f8513b600
Oscar Cosme <oscar.cosme@gmail.com>
2025-06-02 23:27:53 -0300

    Registro de pacientes - Veterinaria

---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,47 @@
+import { toast } from "react-toastify"
+import { usePatientStore } from "../store/store"
+import type { Patient } from "../types"
+import PatientDetailItem from "./PatientDetailItem"
+
+type PatientDetailsProps = {
+    patient: Patient
+}
+
+export default function PatientDetails({patient} : PatientDetailsProps) {
+
+    const deletePatient = usePatientStore( (state) => state.deletePatient )
+    const getPatientById = usePatientStore( (state) => state.getPatientById )
+
+    const handleDelete = () => {
+        deletePatient(patient.id)
+        toast('Paciente eliminado', {
+            type: 'error'
+        })
+    }
+
+    return (
+        <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+            <PatientDetailItem label="ID" data={patient.id} />
+            <PatientDetailItem label='Nombre' data={patient.name} />
+            <PatientDetailItem label='Propietario' data={patient.caretaker} />
+            <PatientDetailItem label='Email' data={patient.email} />
+            <PatientDetailItem label='Fecha Alta' data={patient.date.toString()} />
+            <PatientDetailItem label='Síntomas' data={patient.symptoms} />
+
+            <div className="flex flex-col lg:flex-row gap-3 justify-between mt-10">
+                <button 
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg cursor-pointer"
+                    onClick={() => getPatientById(patient.id)}
+                >
+                    Editar
+                </button>
+                <button 
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg cursor-pointer"
+                    onClick={handleDelete}
+                >
+                    Eliminar
+                </button>
+            </div>
+        </div>
+    )
+}
